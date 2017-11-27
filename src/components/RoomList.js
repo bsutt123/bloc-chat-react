5687c7b01e78a493077d64648beecec636db3bb0
Brady Sutton <bsutt123@gmail.com>
2017-11-27 11:39:31 -0500

    added in room functionality

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,27 +7,38 @@ class RoomList extends Component {
         console.log(props);
         this.roomsRef = this.props.firebase.database().ref('rooms');
         this.state = {
-            rooms: []
+            rooms: [],
+            newRoomName: ''
         }
     }
     componentDidMount() {
         this.roomsRef.on('child_added', snapshot => {
-            console.log(snapshot.val());
             this.setState({rooms: this.state.rooms.concat(snapshot.val().name)});
         })
+    }
+    handleChange(event) {
+        this.setState({newRoomName: event.target.value})
+    }
+    handleClick() {
+        this.roomsRef.push({name: this.state.newRoomName});
+        this.setState({newRoomName: ''})
     }
     render() {
         return (
             <SidePanel> 
                 <Title> Welcome to Bloc Chat! </Title>
                 <SubTitle> Please select your room </SubTitle>
-                <ul>
+                <StyledUl>
                     {
                         this.state.rooms.map((roomName,index ) => {
                             return <RoomItem key={index} roomName={roomName} />
                         })
                     }
-                </ul>
+                </StyledUl>
+                <NewRoomDiv>
+                    <NewRoomInput type="text" value={this.state.newRoomName}  onChange={this.handleChange.bind(this)} />
+                    <SubmitButton onClick={this.handleClick.bind(this)}> + </SubmitButton>
+                </NewRoomDiv>
             </SidePanel>
         )
     }
@@ -38,6 +49,10 @@ const RoomItem = (props) => {
         <StyledLi> {props.roomName} </StyledLi> 
     )
 }
+
+const StyledUl = styled.ul`
+    margin: auto;
+`
 
 const SidePanel = styled.div`
     position: absolute;
@@ -64,6 +79,19 @@ const StyledLi = styled.li`
     color: black;
     cursor: pointer;
     margin: 0.5rem 0 0.5rem 0;
+`
+
+const NewRoomDiv = styled.div`
+    width: 100%;
+    text-align: center;
+`
+const NewRoomInput = styled.input`
+    margin:auto; 
+    width: 60%;
+`
+
+const SubmitButton = styled.button`
+    width: 10%;
 `
 
 export default RoomList;
